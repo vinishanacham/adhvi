f1558012b88421469eb63e3b048654debbfa587c
vinisha <vinishanacham1@gmail.com>
2022-05-27 11:01:07 +0000

    myfile

--- abc.py
+++ abc.py
@@ -1,2 +1,31 @@
-print('hello world')
+class mynum():
 
+    def __iter__(self):
+
+        self.a=1
+
+        return self
+
+    def __next__(self):
+
+        x=self.a
+
+        self.a +=1
+
+        return x
+
+myclass=mynum()
+
+myiter=iter(myclass)
+
+print(next(myclass))
+
+print(next(myclass))
+
+print(next(myclass))
+
+print(next(myclass))
+
+print(next(myclass))
+
+print(next(myclass))
